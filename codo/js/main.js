1a74a6dff37d86c14502ab432cf0a082efef8168
Roque053 <93896718+Roque053@users.noreply.github.com>
2024-04-02 18:07:52 -0300

    Add files via upload

---
+++ codo/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Cargar el contenido de la página actual en la sección de contenido
+    var currentUrl = window.location.href;
+    loadPage(currentUrl);
+});
+
+function loadPage(url) {
+    var content = document.querySelector('.content');
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true); // Cargar el archivo HTML correspondiente a la página
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var responseHTML = xhr.responseText;
+            var parser = new DOMParser();
+            var newDocument = parser.parseFromString(responseHTML, 'text/html');
+            var newContent = newDocument.querySelector('.content').innerHTML;
+            content.innerHTML = newContent; // Insertar el contenido en la sección de contenido
+        }
+    };
+    xhr.send();
+}
